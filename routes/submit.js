7c91d99bc6960a7f5823a6d2db3d1b5864139bb5
Neota Moe <neota.moe@gmail.com>
2017-06-09 14:48:25 -0500

    error handling

--- routes/submit.js
+++ routes/submit.js
@@ -17,8 +17,13 @@ router.post('/', function(req, res) {
     newQuestion.userId = req.user._id;
     newQuestion.userEmail = req.user.email;
     var addQuestion = questionsModel(newQuestion);
-    addQuestion.save().then(function(data){
-      res.sendStatus(200);
+    addQuestion.save(function(err, data){
+      if (err) {
+        console.log('Database Error: ', err);
+        res.sendStatus(500);
+      } else{
+        res.sendStatus(200);
+      }
     });
 
     // create reusable transporter object using the default SMTP transport
@@ -63,33 +68,38 @@ router.put('/:id', function(req, res) {
   if(req.isAuthenticated()) {
     // send back user object from database
     console.log('still logged in');
-    questionsModel.findOneAndUpdate({_id:objectid},{display:'false'}).then(function(){
-      // create reusable transporter object using the default SMTP transport
-      var transporter = nodemailer.createTransport({
-          host: 'smtp.gmail.com',
-          port: 465,
-          secure: true, // secure:true for port 465, secure:false for port 587
-          auth: {
-              user: 'science123app@gmail.com',
-              pass: 'Science123'
-          }
-      });
-      // setup email data with unicode symbols
-      var mailOptions = {
-          from: '"Science123 Quiz" <science123app@gmail.com>',
-          to: '"Sci123App Admin" <science123app@gmail.com>',
-          subject: 'Question added and ready for review',
-          text: 'Hello!  Someone flagged a question to the Science123 Quiz App.  Please log in to the app to review it for approval, denial or editing.', // plain text body
-          html: '<h2>Hello!</h2><p>Someone flagged a question to the <strong>Science123 Quiz App.</strong>  Please log in to the app to review it for approval, denial or editing.</p>' // html body
-      };
-      // send mail with defined transport object
-      transporter.sendMail(mailOptions, function (error, info) {
-          if (error) {
-              return console.log(error);
-          }
-          console.log('Message %s sent: %s', info.messageId, info.response);
-      });
-      res.sendStatus(200);
+    questionsModel.findOneAndUpdate({_id:objectid},{display:'false'}, function(err,data){
+      if (err) {
+        console.log('Database Error: ', err);
+        res.sendStatus(500);
+      } else {
+        // create reusable transporter object using the default SMTP transport
+        var transporter = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true, // secure:true for port 465, secure:false for port 587
+            auth: {
+                user: 'science123app@gmail.com',
+                pass: 'Science123'
+            }
+        });
+        // setup email data with unicode symbols
+        var mailOptions = {
+            from: '"Science123 Quiz" <science123app@gmail.com>',
+            to: '"Sci123App Admin" <science123app@gmail.com>',
+            subject: 'Question added and ready for review',
+            text: 'Hello!  Someone flagged a question to the Science123 Quiz App.  Please log in to the app to review it for approval, denial or editing.', // plain text body
+            html: '<h2>Hello!</h2><p>Someone flagged a question to the <strong>Science123 Quiz App.</strong>  Please log in to the app to review it for approval, denial or editing.</p>' // html body
+        };
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+        });
+        res.sendStatus(200);
+      }
     });
   } else {
     // failure best handled on the server. do redirect here.
@@ -98,6 +108,5 @@ router.put('/:id', function(req, res) {
     res.sendStatus(403);
   }
 });
-
 
 module.exports = router;
