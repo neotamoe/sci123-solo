3018fade04d2484061b2e44be0487090d7c35ad9
Neota Moe <neota.moe@gmail.com>
2019-01-01 20:10:52 -0600

    adding route for potential use in chrome extension

--- routes/questions.js
+++ routes/questions.js
@@ -41,6 +41,23 @@ router.get('/', function(req, res) {
   }
 });
 
+// GET 1 random question (for use in chrome extension)
+// http://sci123.herokuapp.com/#/quiz/extension
+router.get('/extension', function(req, res) {
+  // query to get 1 random question for quiz
+  db.mycoll.aggregate([{ $sample: { size: 1 } }])
+
+  questionsModel.aggregate([{$sample: { size: 1 }}], function(err, data){
+    if (err) {
+      console.log('Database Error: ', err);
+      res.sendStatus(500);
+    } else{
+      console.log(data);
+      res.send(data);
+    }
+  });
+
+});
 
 module.exports = router;
 
