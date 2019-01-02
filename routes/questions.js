0307e9d100ed85e49961dcb2c5d5ca79c88aed40
Neota Moe <neota.moe@gmail.com>
2019-01-01 20:55:06 -0600

    Revert "trying new route for extension"
    This reverts commit 4e0bb1b6062f907b498647058cbe32e29d698399.

--- routes/questions.js
+++ routes/questions.js
@@ -26,6 +26,22 @@ router.get('/:chapter', function(req, res) {
   }
 });
 
+// GET 1 random question (for use in chrome extension)
+// http://sci123.herokuapp.com/#/quiz/extension
+router.get('/extension', function(req, res) {
+  // query to get 1 random question for quiz
+  questionsModel.aggregate([{$sample: { size: 1 }}], function(err, data){
+    if (err) {
+      console.log('Database Error: ', err);
+      res.sendStatus(500);
+    } else{
+      console.log(data);
+      res.send(data);
+    }
+  });
+
+});
+
 // GET tags from database to display
 router.get('/', function(req, res) {
   // check if user is logged in & send back user object from database
