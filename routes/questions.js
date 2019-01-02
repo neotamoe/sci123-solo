2b7e08e5d2eb16966886beda1d7627fb09842490
Neota Moe <neota.moe@gmail.com>
2019-01-01 20:43:41 -0600

    trying different order

--- routes/questions.js
+++ routes/questions.js
@@ -5,22 +5,6 @@ var passport = require('passport');
 var path = require('path');
 var questionsModel = require('../models/questions.model');
 
-// GET 1 random question (for use in chrome extension)
-// http://sci123.herokuapp.com/#/quiz/extension
-router.get('/extension', function(req, res) {
-  // query to get 1 random question for quiz
-  questionsModel.aggregate([{$sample: { size: 1 }}], function(err, data){
-    if (err) {
-      console.log('Database Error: ', err);
-      res.sendStatus(500);
-    } else{
-      console.log(data);
-      res.send(data);
-    }
-  });
-
-});
-
 // GET questions selected by chapter
 router.get('/:chapter', function(req, res) {
   var currentChapter = parseInt(req.params.chapter);
@@ -40,6 +24,22 @@ router.get('/:chapter', function(req, res) {
     $location.path('/home');
     res.sendStatus(403);
   }
+});
+
+// GET 1 random question (for use in chrome extension)
+// http://sci123.herokuapp.com/#/quiz/extension
+router.get('/extension', function(req, res) {
+  // query to get 1 random question for quiz
+  questionsModel.aggregate([{$sample: { size: 1 }}], function(err, data){
+    if (err) {
+      console.log('Database Error: ', err);
+      res.sendStatus(500);
+    } else{
+      console.log(data);
+      res.send(data);
+    }
+  });
+
 });
 
 // GET tags from database to display
