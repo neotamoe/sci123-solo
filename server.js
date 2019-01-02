e3b601554aafbe71270446d1edb1b3fb8aa67a54
Neota Moe <neota.moe@gmail.com>
2019-01-01 20:52:53 -0600

    updating service and routes

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ var box = require ('./routes/box');
 var points = require ('./routes/points');
 var pending = require('./routes/pending');
 var submit = require ('./routes/submit');
+var extension = require('./routes/extension');
 
 // body parser middleware
 app.use(bodyParser.json());
@@ -46,6 +47,7 @@ app.use('/questions/', questions);
 app.use('/points', points);
 app.use('/pending/', pending);
 app.use('/submit', submit);
+app.use('/extension', extension);
 app.use('/*', index);
 
 // mongo connection
